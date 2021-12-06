114e63b149b246bb87381635781168d3ca22020c
Bohdan <bondarbv@icloud.com>
2021-12-06 17:16:29 +0200

    URLSession practice

--- TapToDownloadAnImage/ViewController.swift
+++ TapToDownloadAnImage/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
+    
+    @IBAction func getImagePressed(_ sender: UIButton) {
+        
+        getImageButton.isEnabled = false
+        label.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://rozetked.me/images/uploads/webp/0dak1bQ26FQu.webp") else {return}
 
-
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { data, response, error in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }.resume()
+    }
 }
 
